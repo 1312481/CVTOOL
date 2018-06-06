b87cc17616672e88ff79f6c2836f6d10af76d9d3
HARVEYNASH\sangnguyenq <nguyenquangsang2810@gmail.com>
2018-06-06 11:31:27 +0700

    update education

--- src/components/education.js
+++ src/components/education.js
@@ -6,6 +6,7 @@ import pencil from '../assets/images/pencil.svg'
 import { connect } from 'react-redux'
 import { profileFetchData } from '../actions/profile'
 import { profileUpdateData } from '../actions/profile'
+import '../assets/styles/education.css'
 
 // const API = 'https://api.myjson.com/bins/eoigu'
 
@@ -28,45 +29,54 @@ class Education extends Component {
     //   }
 
     render() {
+        if (this.props.hasErrored) {
+
+            return <div>Sorry! There was an error</div>;
+        }
+        else if (!this.props.isLoaded) {
+
+            return <div>Loading...</div>;
+        }
+        else {
 
 
-        console.log(this.props.profile);
-        return (
+            console.log(this.props.profile);
+            return (
 
-            <div >
-                <div className=" maincontent">
-                    <div className="maincontent__header">Education</div>
-                </div>
-                <div className="maintable">
-                    <table>
-                        <tr className="table-custom">
-                            <th scope="col">Major</th>
-                            <th scope="col">University</th>
-                            <th scope="col">Graduated Year</th>
-                        </tr>
+                <div >
+                    <div className=" maincontent">
+                        <div className="maincontent__header">Education</div>
+                    </div>
+                    <div className="maintable">
+                        <table>
+                            <thead>
+                                <tr className="table-custom">
+                                    <th scope="col">Major</th>
+                                    <th scope="col">University</th>
+                                    <th scope="col">Graduated Year</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{this.props.profile.education[0].name}</td>
+                                    <td>{this.props.profile.education[0].major}</td>
+                                    <td>{this.props.profile.education[0].gradutedTime}</td>
+                                </tr>
+                                <tr>
+                                    <td>{this.props.profile.education[1].name}</td>
+                                    <td>{this.props.profile.education[1].major}</td>
+                                    <td>{this.props.profile.education[1].gradutedTime}</td>
+                                </tr>
+                           
+                            </tbody>
 
-                        {/* <tr>
-                            <td>{{ name }}</td>
-                            <td>{{ major }}</td>
-                            <td>{{ gradutedTime }}</td>
-                        </tr>
-                        <tr>
-                            <td>{{ name }}</td>
-                            <td>{{ major }}</td>
-                            <td>{{ gradutedTime }}</td>
-                        </tr>
-                        <tr>
-                            <td>{{ name }}</td>
-                            <td>{{ major }}</td>
-                            <td>{{ gradutedTime }}</td>
-                        </tr> */}
-
-                    </table>
-                </div>
+                        </table>
+                    </div>
 
 
-            </div >
-        );
+                </div >
+            );
+        }
     }
 }
 
@@ -76,7 +86,8 @@ class Education extends Component {
 const mapStateToProps = (state) => {
     return {
         profile: state.profile,
-
+        hasErrored: state.profileHasErrored,
+        isLoaded: state.profileIsLoaded
 
     };
 };
